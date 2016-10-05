312d8b5072edcafbaed7af96a2e8a382f4705c3c
Simon Herrera <simonherrera.tech@gmail.com>
2016-10-05 00:22:56 -0500

    Responsive Links

--- client/app/app.js
+++ client/app/app.js
@@ -12,11 +12,9 @@ angular.module('vafApp', ['ngRoute'])
 
 var htmlLinks = document.getElementById('nav-links');
 htmlLinks.innerHTML =
-  "<nav class='menu'><ul><li><a href='/#/'>Home</a></li>" +
+  "<ul><li><a href='/#/'>Home</a></li>" +
   "<li><a href='/#/teams'>Teams</a></li>" +
   "<li><a href='/#/players'>Players</a></li>" +
-  "<li><a href='/#/schedule'>Schedule</a></li>" +
   "<li><a href='/#/organization'>Organization</a></li>" +
   "<li><a href='/#/merchandise'>Merchandise</a></li>" +
-  "<li><a href='/#/play-vintage'>Play Vintage</a></li>" +
-  "<li><a href='/#/about'>About</a></li></ul></nav>";
+  "<li><a href='/#/about'>About</a></li></ul>";
