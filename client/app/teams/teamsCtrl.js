3f27ff62cf87d563fea5ef7ec4ea5fc16f35ccfa
Simon Herrera <simonherrera.tech@gmail.com>
2017-01-18 15:56:49 -0600

    Trying to get Managers

--- client/app/teams/teamsCtrl.js
+++ client/app/teams/teamsCtrl.js
@@ -7,15 +7,28 @@ angular.module('vafApp')
     teams.welcome = 'Welcome Vintage Teams'
     teams.intialView = true;
     teams.individualTeamView = false
+    teams.managerView = false
 
     $http.get (apiUrl+'/teams/').then(res => {
       console.log("res", res);
       teams.vafTeams = res.data
     })
 
+    //Select Year and get games and specific game - get players for that year
+    // $http.get (`${apiUrl}/teams/${$routeParams.selectedTeam}/`)
+    // .then(res => {
+    //   console.log("res2", res);
+    //   teams.team = res.data;
+    // }).then(() =>
+    //   $http.get (apiUrl+'/managers/').then(res => {
+    //     team.allManagers = res.data
+    //   })
+    // )
+
     teams.selectTeam = team => {
       teams.intialView = false;
       teams.individualTeamView = true;
+      teams.managerView = true;
       teams.team = team
     }
   });
