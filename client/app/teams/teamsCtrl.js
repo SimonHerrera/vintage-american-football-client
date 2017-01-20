8cb2a41b4ab920adbfc642f829ca1c641c5bc21d
Simon Herrera <simonherrera.tech@gmail.com>
2017-01-19 23:12:20 -0600

    Working on pulling in Managers for Teams

--- client/app/teams/teamsCtrl.js
+++ client/app/teams/teamsCtrl.js
@@ -3,22 +3,26 @@
 angular.module('vafApp')
   .controller('TeamsCtrl', function(apiUrl, $http, $routeParams, $anchorScroll) {
 
-    const teams = this
-    teams.welcome = 'Welcome Vintage Teams'
-    teams.intialView = true;
-    teams.individualTeamView = false
-    // part of view but do I need seperate managerView here because it's part of the teamView?
-    teams.managerView = false
+    $anchorScroll.yOffset=200
 
+    const teams = this;
+    teams.welcome = 'Welcome Vintage Teams';
+    teams.intialView = true;
+    teams.individualTeamView = false;
+    // part of view but do I need seperate managerView here because it's part of the teamView?
+    teams.managerView = false;
+
+    // Get Teams into controller
     $http.get (apiUrl+'/teams/').then(res => {
       console.log("res", res);
       teams.allTeams = res.data
     })
 
-    //Select Team and get manager from that team
-    // $http.get (`${apiUrl}/teams/${$routeParams.selectedTeam}/`)
-    // .then(res => {
-    //   console.log("res2", res);
+    // Select Team and get manager from that team
+    $http.get (`${apiUrl}/teams/${$routeParams.selectedTeam}/`)
+    .then(res => {
+      console.log("res2", res);
+    })
     //   teams.team = res.data;
     // }).then(() =>
     //   $http.get (apiUrl+'/managers/').then(res => {
@@ -30,6 +34,6 @@ angular.module('vafApp')
       teams.intialView = false;
       teams.individualTeamView = true;
       teams.managerView = true;
-      teams.team = team
+      teams.team = team;
     }
   });
