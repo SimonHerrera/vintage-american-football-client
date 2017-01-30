fc756f7e45207830f3db3a511f31238f0dc35544
Simon Herrera <simonherrera.tech@gmail.com>
2017-01-30 11:48:17 -0600

    Update site font size - added future code for mobile nav

--- client/app/app.js
+++ client/app/app.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('vafApp', ['ngRoute'])
-  .constant('apiUrl', "http://localhost:8000")
-  // .constant('apiUrl', "http://104.236.6.142:8001") // something similar for server
+  // .constant('apiUrl', "http://localhost:8000")
+  .constant('apiUrl', "http://104.236.6.142:8001") // something similar for server
 
   .config($httpProvider => {
     $httpProvider.defaults.xsrfCookieName = 'csrftoken'; // adds header
@@ -19,3 +19,30 @@ htmlLinks.innerHTML =
   "<li><a href='/#/organization'>Organization</a></li>" +
   "<li><a href='/#/merchandise'>Merchandise</a></li>" +
   "<li><a href='/#/about'>About</a></li></ul>";
+
+// var htmlLinks = document.getElementById('nav-links');
+// htmlLinks.innerHTML =
+//   '<nav class="navbar navbar-inverse navbar-fixed-top">' +
+//     '<div class="container">' +
+//       '<div class="navbar-header">' +
+//         '<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">' +
+//           '<span class="sr-only">Toggle navigation</span>' +
+//           '<span class="icon-bar"></span>' +
+//           '<span class="icon-bar"></span>' +
+//           '<span class="icon-bar"></span>' +
+//         '</button>' +
+//         '<a class="navbar-brand" href="../index.html">Vintage American Football</a>' +
+//       '</div>' +
+//       '<div id="navbar" class="collapse navbar-collapse">' +
+//         '<ul class="nav navbar-nav">' +
+//           '<li><a href="/#/">Home</a></li>' +
+//           '<li><a href="/#/teams">Teams</a></li>' +
+//           '<li><a href="/#/players">Players</a></li>' +
+//           '<li><a href="/#/schedules">Scheduels</a></li>' +
+//           '<li><a href="/#/organization">Organization</a></li>' +
+//           '<li><a href="/#/merchandise">Merchandise</a></li>' +
+//           '<li><a href="/#/about">About</a></li>' +
+//         '</ul>' +
+//       '</div>' +
+//     '</div>' +
+//   '</nav>';
