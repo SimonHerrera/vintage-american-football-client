71e070285f5626a200b44114375eb3965551025c
Simon Herrera <simonherrera.tech@gmail.com>
2017-02-01 01:08:14 -0600

    Update Mobile View to show player info below scroll div - Added Clear Filter

--- client/app/players/playersCtrl.js
+++ client/app/players/playersCtrl.js
@@ -1,20 +1,28 @@
 'use strict';
 
 angular.module('vafApp')
-  .controller('PlayersCtrl', function(apiUrl, $http, $routeParams, $anchorScroll) {
+  .controller('PlayersCtrl', function($scope, apiUrl, $http, $routeParams, $anchorScroll) {
 
     $anchorScroll.yOffset=500
+
+    $scope.clearFilter = function() {
+      $scope.userSearch = ""
+    }
 
     const players = this
     players.welcome = 'Vintage Football Players'
     players.intialView = true;
     players.individualPlayerView = false
 
+    // players.clearFilter = function() {
+    //   players.userSearch = ""
+    // }
 
     $http.get (apiUrl+'/players/').then(res => {
       console.log("res", res);
       players.vafPlayers = res.data
     })
+
 
 
     //when run do this
