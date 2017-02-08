1538917b8e842b5f1f13408a01a5d0e5aff7726f
Simon Herrera <simonherrera.tech@gmail.com>
2017-02-08 01:15:55 -0600

    Remove Home from Menu and add VAF

--- client/app/app.js
+++ client/app/app.js
@@ -21,11 +21,11 @@ htmlLinks.innerHTML =
           '<span class="icon-bar"></span>' +
           '<span class="icon-bar"></span>' +
         '</button>'+
-        // '<a class="navbar-brand" href="../index.html">Vintage American Football</a>' +
+        '<a class="navbar-brand" href="../index.html">V A F</a>' +
       '</div>' +
       '<div id="navbar" class="collapse navbar-collapse">' +
         '<ul class="nav navbar-nav">' +
-    "<li><a href='/#/'>Home</a></li>" +
+    // "<li><a href='/#/'>Home</a></li>" +
 "<li><a href='/#/teams'>Teams</a></li>" +
   "<li><a href='/#/players'>Players</a></li>" +
   "<li><a href='/#/schedules'>Schedules</a></li>" +
