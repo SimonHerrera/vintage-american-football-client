2fe529e122858cbbd035156eb8e860f6c38881e3
Simon Herrera <simonherrera.tech@gmail.com>
2017-07-16 22:42:23 -0500

    Fixed JS Page

--- client/app/landing/landingCtrl.js
+++ client/app/landing/landingCtrl.js
@@ -1,10 +1,10 @@
-// 'use strict';
+'use strict';
 
-// angular.module('vafApp')
-//   .controller('LandingCtrl', function(apiUrl, $http) {
+angular.module('vafApp')
+  .controller('LandingCtrl', function(apiUrl, $http) {
 
-//     const landing = this
+    const landing = this
 
-//     landing.welcome = 'Welcome to the page'
+    landing.welcome = 'Welcome to the page'
 
-//   });
+  });
